f52cc6abc62c565c258b80c94f105a384df62514
Ppyt_win <onwukaprince99@gmail.com>
2024-11-20 14:50:44 -0800

    welcome

--- hello.py
+++ hello.py
@@ -1,27 +1,27 @@
-# # # print('f hello {name_of_students:}')
-# # # name_of_students = input(('enter your name:'))
+# # # # print('f hello {name_of_students:}')
+# # # # name_of_students = input(('enter your name:'))
 
-# # # pass_mark = int(input('enter your score:'))
-# # # if pass_mark > 50 or pass_mark == 50:
-#     print(f'hi {name_of_students} you passed')
-# # a, b = 10,20
-# # if 'foo' in ['foo','bar','wat']:
-# #     print('innit')
-# #     if a > b:
-# #         print('true')
-# #     else:
-# #         print('false')
+# # # # pass_mark = int(input('enter your score:'))
+# # # # if pass_mark > 50 or pass_mark == 50:
+# #     print(f'hi {name_of_students} you passed')
+# # # a, b = 10,20
+# # # if 'foo' in ['foo','bar','wat']:
+# # #     print('innit')
+# # #     if a > b:
+# # #         print('true')
+# # #     else:
+# # #         print('false')
     
-# #     print('done')
+# # #     print('done')
 
-# #     if a < b:
-# #         print(ce*20/100
-# print('you are given $160 as discount')
-# if Amount <100:
-#     print('no discount')'yes')
+# # #     if a < b:
+# # #         print(ce*20/100
+# # print('you are given $160 as discount')
+# # if Amount <100:
+# #     print('no discount')'yes')
     
-# #     print('another one')
-# # print('the end')
+# # #     print('another one')
+# # # print('the end')
 
 
 
@@ -37,6 +37,26 @@ elif Amount ==100 or Amount <=500:
     print(f'Hello, customer! You have a 10% discount of $ {discount} and a 5% VAT of ${Tax}. You are to pay ${Amount-discount+Tax} Thanks.')
 elif Amount >500:
     print(f'Hello, customer! You have a 20% discount of ${discount} and an 8% VAT of ${Tax}. You are to pay ${Amount-discount+Tax}Thanks')
+
+
+# greetings = 'welcome to learnFactory'
+# Students_name =input(f'enter your name: ')
+# print(f'{Students_name}! {greetings}')
+# exam_score = float(input('enter your exam score: '))
+
+# if exam_score <=0 or exam_score > 100:
+#     print(f'{Students_name} your {exam_score} is an invalid score')
+# else:
+#     if exam_score >=90 or exam_score ==100:
+#         print(f'{Students_name} your exam score is {exam_score}. you got A!')
+#     elif  exam_score ==80 or exam_score <=89:
+#          print(f'{Students_name} your exam score is {exam_score}. you got B!')
+#     if exam_score ==70 or exam_score <=79:
+#          print(f'{Students_name} your exam score is {exam_score}. you got C!')
+#     if exam_score ==60 or exam_score <=69:
+#          print(f'{Students_name} your exam score is {exam_score}. you got D!')
+#     if exam_score <60:
+#          print(f'{Students_name} your exam score is {exam_score}. you got F!')
 
     
 
